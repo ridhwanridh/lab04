7286f7f11b9532a2334e4c5aca83a1971e0b0c0c
ridhwanridh <137137909+ridhwanridh@users.noreply.github.com>
2024-04-17 23:58:42 -0400

    Add files via upload

--- js/json.js
+++ js/json.js
@@ -7,14 +7,14 @@ async function populate() {
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4a: Create i-scream.json file with companyName, headOffice, established, active, topFlavors(name, calories, type, ingredients, image) */
     // STEP 4b: Store the URL of a JSON file in a variable */
-    const url = "https://priyansht.github.io/24W-JavaScript-03-Week11/js/i-scream.json";
+    const url = "https://ridhwanridh.github.io/lab04//js/i-scream.json";
     // STEP 5: Use the new URL to create a new request object
     const request = new Request(url);
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
     const response = await fetch(request);
     // STEP 7: Capture the returned Response object and covert to a JSON object using json()
     const responseJson = await response.json();
-    // STEP 8: Output the iScream JSON object to the console 
+    // STEP 8: Output the iScream JSON object to the console
     console.log(responseJson);
     // STEP 9a: Invoke the populateHeader function here, then build it below
     populateHeader(responseJson);
@@ -37,47 +37,73 @@ function populateHeader(jsonData) {
 
 /* STEP 10b: Assemble the showTopFlavors() function */
 function showTopFlavors(jsonData) {
-    // STEP 10c: Bind the JSON topFlavors object to a var
     const topFlavors = jsonData.topFlavors;
-    // STEP 10d: Loop through the topFlavors object
+
+    // Creating tab list
+    const tabList = document.createElement("ul");
+    tabList.className = 'tab-list';
+
+    // Creating container for tab content
+    const tabContent = document.createElement("div");
+    tabContent.className = 'tab-content';
+
     for (let i = 0; i < topFlavors.length; i++) {
-        console.log(topFlavors[i]);
-        // STEP 10e: build HTML elements for the content: article, h2, image, p1, p2, list
-        let article = document.createElement("article"); // <article></article>
-        let h2 = document.createElement("h2"); // <h2></h2>
-        let p1 = document.createElement("p"); // <p></p>
-        let p2 = document.createElement("p"); // <p></p>
-        let image = document.createElement("img"); // <img>
-        let list = document.createElement("ul"); // <ul></ul>
-        // STEP 10f: Set the textContent property for each of the above elements (except the UL), based on the JSON content
+        // Tab buttons
+        const tabButton = document.createElement("button");
+        tabButton.className = 'tab';
+        tabButton.textContent = topFlavors[i].name;
+        tabButton.onclick = function () {
+            openTab(event, 'flavor' + i);
+        };
+        const li = document.createElement("li");
+        li.appendChild(tabButton);
+        tabList.appendChild(li);
+
+        // Content for each tab
+        const flavorContent = document.createElement("div");
+        flavorContent.id = 'flavor' + i;
+        flavorContent.className = 'tab-pane';
+
+        let h2 = document.createElement("h2");
+        let image = document.createElement("img");
+        let p1 = document.createElement("p");
+        let p2 = document.createElement("p");
+        let list = document.createElement("ul");
+
         h2.textContent = topFlavors[i].name;
         p1.textContent = "Calories: " + topFlavors[i].calories;
         p2.textContent = "Type: " + topFlavors[i].type;
-        image.setAttribute("src", topFlavors[i].image); // <img src="path to the image">
-        // STEP 10g: Build a loop for the ingredients array in the JSON
-        let ingredients = topFlavors[i].ingredients;
-        for (let j = 0; j < ingredients.length; j++) {
-            console.log(ingredients[j]);
-            let listItem = document.createElement("li"); // <li></li>
-            listItem.textContent = ingredients[j];
-            /*
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-            */
-            list.appendChild(listItem);
-        }
-        // STEP 10i: Append each complete ARTICLE element to the SECTION element
-        article.appendChild(h2);
-        article.appendChild(p1);
-        article.appendChild(p2);
-        article.appendChild(list);
-        article.appendChild(image);
+        image.src = topFlavors[i].image;
 
-        section.appendChild(article);
+        flavorContent.appendChild(h2);
+        flavorContent.appendChild(image);
+        flavorContent.appendChild(p1);
+        flavorContent.appendChild(p2);
+
+        topFlavors[i].ingredients.forEach(ingredient => {
+            let listItem = document.createElement("li");
+            listItem.textContent = ingredient;
+            list.appendChild(listItem);
+        });
+
+        flavorContent.appendChild(list);
+        tabContent.appendChild(flavorContent);
     }
+
+    section.appendChild(tabList);
+    section.appendChild(tabContent);
+}
+function openTab(evt, tabName) {
+    const tabPanes = document.getElementsByClassName("tab-pane");
+    for (let i = 0; i < tabPanes.length; i++) {
+        tabPanes[i].style.display = "none";
+    }
+    const tabs = document.getElementsByClassName("tab");
+    for (let i = 0; i < tabs.length; i++) {
+        tabs[i].className = tabs[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
 
 // STEP 11: Add a 3rd flavour of ice cream to the local JSON file, making use of the /images/strawberry-sprinkle.svg image
